d6ea4a450cc9f5d6d4786e94384b44485a423385
Andries <andriesklopperss@gmail.com>
2020-01-20 12:25:11 +0200

    Fixed tests

--- test/tests/MinifyAndResize.spec.js
+++ test/tests/MinifyAndResize.spec.js
@@ -1,9 +1,9 @@
-const resizer = require('../../index');
+// const resizer = require('../../index');
 
-resizer('./input', './output', [1920, 1080, 480])
-  .then(() => {
-    console.log('Done!');
-  })
-  .catch(err => {
-    console.log('Error:', err);
-  });
+// resizer('./input', './output', [1920, 1080, 480])
+//   .then(() => {
+//     console.log('Done!');
+//   })
+//   .catch(err => {
+//     console.log('Error:', err);
+//   });
--- test/tests/MinifySpec.js
+++ test/tests/MinifySpec.js
@@ -1,43 +1,43 @@
-// describe('Minify', async () => {
-//   const path = require('path');
-//   const fs = require('fs-extra');
-//   const { promisify } = require('util');
-//   const { expect } = require('chai');
+describe('Minify', async () => {
+  const path = require('path');
+  const fs = require('fs-extra');
+  const { promisify } = require('util');
+  const { expect } = require('chai');
 
-//   const { isDir } = require('../../lib/IsDir');
-//   const exists = promisify(fs.exists);
+  const { isDir } = require('../../lib/IsDir');
+  const exists = promisify(fs.exists);
 
-//   const { getMetadata, clearFiles, copyFiles } = require('../helpers');
-//   const minify = require('../../minify');
+  const { getMetadata, clearFiles, copyFiles } = require('../helpers');
+  const minify = require('../../minify');
 
-//   // let inputPath;
-//   // let outputPath;
+  // let inputPath;
+  // let outputPath;
 
-//   let inputPath = path.resolve('./input') + path.sep;
-//   let outputPath = path.resolve('./output');
+  let inputPath = path.resolve('./input') + path.sep;
+  let outputPath = path.resolve('./output');
 
-//   if (!(isDir(inputPath) && isDir(outputPath))) {
-//     throw new Error('Input and/or Output folders do not exist');
-//   }
+  if (!(isDir(inputPath) && isDir(outputPath))) {
+    throw new Error('Input and/or Output folders do not exist');
+  }
 
-//   beforeEach(async function() {
-//     await clearFiles(inputPath);
-//     await clearFiles(outputPath);
-//     await copyFiles(path.resolve('./test_input'), inputPath);
-//   });
+  beforeEach(async function() {
+    await clearFiles(inputPath);
+    await clearFiles(outputPath);
+    await copyFiles(path.resolve('./test_input'), inputPath);
+  });
 
-//   it('Images should be minified and be smaller file size', async function() {
-//     this.timeout(30000);
+  it('Images should be minified and be smaller file size', async function() {
+    this.timeout(30000);
 
-//     const beforeMetadata = await getMetadata(inputPath);
+    const beforeMetadata = await getMetadata(inputPath);
 
-//     await minify(inputPath, outputPath);
+    await minify(inputPath, outputPath);
 
-//     const afterMetadata = await getMetadata(outputPath);
+    const afterMetadata = await getMetadata(outputPath);
 
-//     for (const image in afterMetadata) {
-//       const difference = afterMetadata[image].size - beforeMetadata[image].size;
-//       expect(difference).to.be.lessThan(0);
-//     }
-//   });
-// });
+    for (const image in afterMetadata) {
+      const difference = afterMetadata[image].size - beforeMetadata[image].size;
+      expect(difference).to.be.lessThan(0);
+    }
+  });
+});
